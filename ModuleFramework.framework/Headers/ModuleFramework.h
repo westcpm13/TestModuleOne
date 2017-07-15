03280246585a844b63fe2d346281d9f68260d8c7
Pawel Trojan <paweltrojan@MacBook-Pro-Pawel.local>
2017-07-15 13:08:33 +0200

    initiazlize projects

---
+++ ModuleFramework.framework/Headers/ModuleFramework.h
@@ -0,0 +1,19 @@
+//
+//  ModuleFramework.h
+//  ModuleFramework
+//
+//  Created by Pawel Trojan on 13.07.2017.
+//  Copyright © 2017 Pawel Trojan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ModuleFramework.
+FOUNDATION_EXPORT double ModuleFrameworkVersionNumber;
+
+//! Project version string for ModuleFramework.
+FOUNDATION_EXPORT const unsigned char ModuleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModuleFramework/PublicHeader.h>
+
+#import <ModuleFramework/KlasaObjcFramework.h>
